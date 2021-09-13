f9f2aff99ac39808b173eb0629fe28bc95437272
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-13 20:10:28 +0530

    REFACTOR: passed lambda expression as a function parameter

--- MathOperationApp/src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
+++ MathOperationApp/src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
@@ -5,16 +5,19 @@ public class MathOperationApp {
 	@FunctionalInterface
 	interface IMathFunction{
 		int calculate(int a, int b);
+		static void printResult(String operation, int a, int b, IMathFunction function) {
+			System.out.println(operation+" : "+function.calculate(a, b));
+		}
 	}
 	public static void main(String[] args) {
 		IMathFunction add = (a,b) -> a+b;
 		IMathFunction subtract = (a,b) -> a-b;
 		IMathFunction division = (a,b) -> a/b;
 		
-		System.out.println("Addition: "+add.calculate(3, -5));
-		System.out.println("Subtraction: "+subtract.calculate(9, 4));
-		System.out.println("Division: "+division.calculate(45, 5));
-
+		IMathFunction.printResult("Addition", 3, -5, add);
+		IMathFunction.printResult("Subtraction", 9, 4, subtract);
+		IMathFunction.printResult("Division", 45, 5, division);
+		
 	}
 
 }
