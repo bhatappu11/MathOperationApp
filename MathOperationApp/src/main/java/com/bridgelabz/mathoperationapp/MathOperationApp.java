27dce4bb0c2b93bbcec42831e86dc046f759d0a6
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-13 20:02:42 +0530

    ADDED: lambda function to perform addition,subtraction and division

--- MathOperationApp/src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
+++ MathOperationApp/src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
@@ -2,8 +2,18 @@ package com.bridgelabz.mathoperationapp;
 
 public class MathOperationApp {
 
+	@FunctionalInterface
+	interface IMathFunction{
+		int calculate(int a, int b);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		IMathFunction add = (a,b) -> a+b;
+		IMathFunction subtract = (a,b) -> a-b;
+		IMathFunction division = (a,b) -> a/b;
+		
+		System.out.println("Addition: "+add.calculate(3, -5));
+		System.out.println("Subtraction: "+subtract.calculate(9, 4));
+		System.out.println("Division: "+division.calculate(45, 5));
 
 	}
 
---
+++ MathOperationApp/src/test/java/com/bridgelabz/mathoperationapp/MathOperationAppTest.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.mathoperationapp;
+public class MathOperationAppTest {
+
+}
