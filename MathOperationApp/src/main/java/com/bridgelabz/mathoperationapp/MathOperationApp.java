10c53a5ab2f31137d8f5b568bf0a9e7c33c0f309
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-13 22:17:32 +0530

    MODIFIED: functional interface is placed outside the class

--- MathOperationApp/src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
+++ MathOperationApp/src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
@@ -1,14 +1,15 @@
 package com.bridgelabz.mathoperationapp;
 
+@FunctionalInterface
+interface IMathFunction{
+	int calculate(int a, int b);
+	static void printResult(String operation, int a, int b, IMathFunction function) {
+		System.out.println(operation+" : "+function.calculate(a, b));
+	}
+}
+
 public class MathOperationApp {
 
-	@FunctionalInterface
-	interface IMathFunction{
-		int calculate(int a, int b);
-		static void printResult(String operation, int a, int b, IMathFunction function) {
-			System.out.println(operation+" : "+function.calculate(a, b));
-		}
-	}
 	public static void main(String[] args) {
 		IMathFunction add = (a,b) -> a+b;
 		IMathFunction subtract = (a,b) -> a-b;
